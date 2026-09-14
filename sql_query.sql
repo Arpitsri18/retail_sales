72cfe71a7b149c5b487ea800b05592d1e9532d3d
Arpit Srivastava <arpitsrivastava7921@gmail.com>
2026-09-14 16:26:32 +0530

    Add retail sales dataset

--- sql_query.sql
+++ sql_query.sql
@@ -127,3 +127,85 @@ SELECT
 	SUM(total_sale) AS total_orders
 FROM retail_sales
 GROUP BY category;
+
+-- 4. nd the average age of customers who purchased items from the Beauty category.
+
+SELECT AVG(age)
+FROM retail_sales
+WHERE category = 'Beauty';
+
+-- 5. Transactions where total sale is greater than 1000?
+
+SELECT *
+FROM retail_sales
+WHERE total_sale >1000;
+
+-- 6. Find the total number of transactions made by each gender in each category.
+
+SELECT 
+	category,
+	gender,
+	COUNT(*) AS total_transactions
+FROM retail_sales
+GROUP BY category,gender
+ORDER BY category;
+
+-- 7. Calculate the average sale for each month and find the best-selling month in each year.
+
+SELECT
+    year,
+    month,
+    avg_sale
+FROM
+(
+    SELECT
+        EXTRACT(YEAR FROM sale_date) AS year,
+        EXTRACT(MONTH FROM sale_date) AS month,
+        AVG(total_sale) AS avg_sale,
+        RANK() OVER (
+            PARTITION BY EXTRACT(YEAR FROM sale_date)
+            ORDER BY AVG(total_sale) DESC
+        ) AS rank
+    FROM retail_sales
+    GROUP BY 1, 2
+) AS t1
+WHERE rank = 1;
+
+
+-- 8.  Find the top 5 customers based on the highest total sales.
+
+SELECT
+    customer_id,
+    SUM(total_sale) AS total_sales
+FROM retail_sales
+GROUP BY customer_id
+ORDER BY total_sales DESC
+LIMIT 5;
+
+-- 9. Find the number of unique customers who purchased items from each category.
+
+SELECT
+    category,
+    COUNT(DISTINCT customer_id) AS unique_customers
+FROM retail_sales
+GROUP BY category;
+
+-- 10. Create each shift and find the number of orders.
+
+WITH hourly_sale AS
+(
+    SELECT *,
+        CASE
+            WHEN EXTRACT(HOUR FROM sale_time) < 12
+                THEN 'Morning'
+            WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17
+                THEN 'Afternoon'
+            ELSE 'Evening'
+        END AS shift
+    FROM retail_sales
+)
+SELECT
+    shift,
+    COUNT(*) AS total_orders
+FROM hourly_sale
+GROUP BY shift;
